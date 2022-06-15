007b0ba2b599091d2e8ec9dd588fe026cd1851f3
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-15 14:20:28 -0400

    project initialized

---
+++ app/src/main/java/com/nirmalbhetwal/fa_nirmalbhetwal_c0841296_android/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.nirmalbhetwal.fa_nirmalbhetwal_c0841296_android.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
